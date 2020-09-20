b6613a55a77e084b3489b5bb749660a44e914850
Reddersc022 <64492350+Reddersc022@users.noreply.github.com>
2020-09-20 16:30:46 +0100

    C@H_1 - re-upload

--- Code@Homes/C@H_1.py
+++ Code@Homes/C@H_1.py
@@ -9,3 +9,19 @@ digits = int(input("Digits: "))
 
 for i in range(int(.999999999 * (10 ** digits)) + 1):
 	print(("0" * (digits - len(str(i))) if len(str(i)) < digits else "") + str(i))
+
+# Re-upload - alternative interpretation of instructions
+
+import itertools
+
+"""
+Hierarchical chart:
+		:: Inputs    : Digits
+Program :: Processes : Loop through every possible combo of inputs
+		:: Outputs   : All possible pins
+"""
+
+digits = input("Digits known (space separated): ").split()
+
+print("\n".join(["".join(i) for i in itertools.permutations(digits)]))
+
