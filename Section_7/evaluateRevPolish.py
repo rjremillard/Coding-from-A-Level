c6aa63fae3604fc632cfed0b2c0565b816cea155
Reddersc022 <64492350+Reddersc022@users.noreply.github.com>
2020-10-15 09:31:17 +0100

    Reverse Polish

---
+++ Section_7/evaluateRevPolish.py
@@ -0,0 +1,16 @@
+"""
+Evaluating Reverse Polish Notation
+"""
+
+from math import ceil
+
+expr = input("Expression, space separated: ").split()
+stack = []
+
+for n in expr:
+	if n.isnumeric():
+		stack.insert(0, n)
+	else:
+		stack.insert(0, eval("%s%s%s" % (stack.pop(), n, stack.pop())))
+
+print(stack[0])
