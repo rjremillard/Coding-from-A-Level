ee4838661f8c273f58ea7ea8d3e1256217f3c92e
Reddersc022 <64492350+Reddersc022@users.noreply.github.com>
2020-10-21 19:30:49 +0100

    working on expressionTree.py

--- Section_7/expressionTree.py
+++ Section_7/expressionTree.py
@@ -5,10 +5,18 @@ Evaluates an expression tree
 
 # Get input
 tree = [input("Root\n> ")]
-while tree[-1] in "+-/*":
-	tree.extend([input("Left child\n> "), input("Right child\n> ")])
+while len([i for i in tree if i in "+-/*"]) != len([i for i in tree if i.isnumeric()]) - 1:
+	node = input("%s child\n> " % ("Left" if tree[-1] in "+-/*" else "Right"))
+	tree.append(node)
+
+print(tree)
 
 # Make postfix expression
-expr = [tree[0]]
+expr = []
 for i in range(1, len(tree)-1):
-	expr.append(tree[i])
+	if tree[i].isnumeric() and tree[i+1].isnumeric():
+		expr.extend([tree[i], tree[i+1], tree[i-1]])
+		# TODO: Fix
+
+expr.append(tree[0])
+print(expr)
