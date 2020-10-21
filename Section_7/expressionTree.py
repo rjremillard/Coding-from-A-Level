ac88b3c37bec3d3c652b7cdd3090ff2b3dd75a59
Reddersc022 <64492350+Reddersc022@users.noreply.github.com>
2020-10-21 19:45:19 +0100

    working on expressionTree.py

--- Section_7/expressionTree.py
+++ Section_7/expressionTree.py
@@ -1,11 +1,14 @@
 """
-Evaluates an expression tree
+Nicely: inputs, evaluates, and outputs an expression tree in postfix / reverse Polish notation
 """
 
 
-# Get input
+# Get root
 tree = [input("Root\n> ")]
+# If <amount of operators> = <amount of numbers> - 1, tree is complete
+# Input ends up being prefix
 while len([i for i in tree if i in "+-/*"]) != len([i for i in tree if i.isnumeric()]) - 1:
+	# If node before was operator, next should be left child, otherwise, right
 	node = input("%s child\n> " % ("Left" if tree[-1] in "+-/*" else "Right"))
 	tree.append(node)
 
@@ -13,10 +16,12 @@ print(tree)
 
 # Make postfix expression
 expr = []
-for i in range(1, len(tree)-1):
-	if tree[i].isnumeric() and tree[i+1].isnumeric():
-		expr.extend([tree[i], tree[i+1], tree[i-1]])
+# Skip root
+for i in range(1, len(tree)):
+	if tree[i].isnumeric() or tree[i-1].isnumeric():
+		expr.append(tree[i])
 		# TODO: Fix
 
+# Add root at end
 expr.append(tree[0])
 print(expr)
