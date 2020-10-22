aef5728c52227367c890144f35179726c17ce0da
Reddersc022 <64492350+Reddersc022@users.noreply.github.com>
2020-10-22 19:45:30 +0100

    working on Pre-In-Postfix.py

---
+++ Section_7/Pre-In-Postfix.py
@@ -0,0 +1,130 @@
+"""
+Two holders:
+
+'Conversions', holding functions to convert between Prefix, Infix, and Postfix, which are:
+	- Prefix    -> Infix
+	- Prefix    -> Postfix
+	- Infix     -> Prefix
+	- Infix     -> Postfix
+	- Postfix   -> Prefix
+	- Postfix   -> Infix
+
+'Evaluate', holding functions to evaluate each of Prefix, Infix, and Postfix, which are:
+	- evaluatePrefix
+	- evaluateInfix
+	- evaluatePostfix
+"""
+
+
+# Stack class, acting as data type, for conversions
+# Specifically made for this, so won't have all normal stack methods
+# Also dynamic for ease of use
+class Stack:
+	def __init__(self) -> None:
+		self.s = []
+		self.size = 0
+
+	def pop(self) -> str:
+		if not self.size:
+			raise IndexError("Stack is empty")
+
+		else:
+			self.size -= 1
+			return self.s.pop(0)
+
+	def pop2(self) -> list:
+		return [self.pop() for _ in range(2)]
+
+	def push(self, *args) -> None:
+		self.s.insert(0, "".join(args))
+		self.size += 1
+
+	def return_(self) -> str:
+		return self.s[0]
+
+
+class Conversions:
+	@staticmethod
+	def PrefixToInfix(prefix: str) -> str:
+		infix = Stack()
+		for i in prefix[::-1].split():
+			if i.isnumeric():
+				infix.push(i)
+			else:
+				n0, n1 = infix.pop2()
+				infix.push("(", n0, i, n1, ")")
+
+		return infix.return_()
+
+	@staticmethod
+	def PrefixToPostfix(prefix: str) -> str:
+		postfix = Stack()
+		for i in prefix[::-1].split():
+			if i.isnumeric():
+				postfix.push(i)
+			else:
+				n0, n1 = postfix.pop2()
+				postfix.push(n0, n1, i)
+
+		return postfix.return_()
+
+	@staticmethod
+	def InfixToPrefix(infix: str) -> str:
+		# TODO: InfixToPrefix
+		pass
+
+	@staticmethod
+	def InfixToPostfix(infix: str) -> str:
+		# TODO: InfixToPostfix
+		pass
+
+	@staticmethod
+	def PostfixToPrefix(postfix: str) -> str:
+		prefix = Stack()
+		for i in postfix.split():
+			if i.isnumeric():
+				prefix.push(i)
+			else:
+				n0, n1 = prefix.pop2()
+				prefix.push(i, n1, n0)
+
+		return prefix.return_()
+
+	@staticmethod
+	def PostfixToInfix(postfix: str) -> str:
+		infix = Stack()
+		for i in postfix.split():
+			if i.isnumeric():
+				infix.push(i)
+			else:
+				n0, n1 = infix.pop2()
+				infix.push("(", n1, i, n0, ")")
+
+		return infix.return_()
+
+
+class Evaluates:
+	@staticmethod
+	def evaluatePrefix(prefix: str) -> int:
+		# TODO: evaluatePrefix
+		pass
+
+	@staticmethod
+	def evaluateInfix(infix: str) -> int:
+		# TODO: evaluateInfix
+		pass
+
+	@staticmethod
+	def evaluatePostfix(postfix: str) -> int:
+		# TODO: evaluatePostfix
+		pass
+
+
+# Testcases
+if __name__ == "__main__":
+	print("--\nPrefix: + * 2 4 / 6 3\nTranslated Infix: %s\nTranslated Postfix: %s\nEvaluated: %d\n--"
+							% (Conversions.PrefixToInfix("+ * 2 4 / 6 3"), Conversions.PrefixToPostfix("+ * 2 4 / 6 3"), 0))
+	print("--\nInfix: 2 * (4 + 6) / 3\nTranslated Prefix: %s\nTranslated Postfix: %s\nEvaluated: %d\n--"
+							% ("None", "None", 0))
+	print("--\nPostfix: 2 4 6 3 / + *\nTranslated Prefix: %s\nTranslated Infix: %s\nEvaluated: %d\n--"
+							% (Conversions.PostfixToPrefix("+ * 2 4 / 6 3"), Conversions.PostfixToInfix("+ * 2 4 / 6 3"), 0))
