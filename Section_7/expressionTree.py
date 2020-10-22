14a4632d732ab5ed83b2bfefcdf249d54fc28677
Reddersc022 <64492350+Reddersc022@users.noreply.github.com>
2020-10-22 15:11:08 +0100

    finished expressionTree.py

--- Section_7/expressionTree.py
+++ Section_7/expressionTree.py
@@ -3,7 +3,6 @@ Nicely: inputs, evaluates, and outputs an expression tree in postfix / reverse P
 Currently only works with depth 2
 """
 
-
 # Get root
 tree = [input("Root\n> ")]
 # If <amount of operators> = <amount of numbers> - 1, tree is complete
@@ -16,8 +15,9 @@ while len([i for i in tree if i in "+-/*"]) != len([i for i in tree if i.isnumer
 # Save prefix
 prefix = "".join(tree)
 
+
 # Make infix expression
-infixStack = tree[::-1]
+infixStack = []
 
 for i in tree[::-1]:
 	# If number, push to stack
@@ -29,6 +29,22 @@ for i in tree[::-1]:
 		infixStack.insert(0, "(%s%s%s)" % (n0, i, n1))
 
 # Save infix
-infix = infixStack[0]
+infix = "".join(infixStack)
 
-print(prefix, infix)
+
+# Make postfix expression
+postfixStack = []
+
+for i in tree[::-1]:
+	# If number, push to stack
+	if i.isnumeric():
+		postfixStack.append(i)
+	# If operator, sort out
+	else:
+		n0, n1 = postfixStack.pop(), postfixStack.pop()
+		postfixStack.append(n0+n1+i)
+
+# Save postfix
+postfix = "".join(postfixStack)
+
+print(prefix, infix, postfix, sep="\n")
