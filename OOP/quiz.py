677fd8e19e66712405506c150d267e07e63a083a
Reddersc022 <64492350+Reddersc022@users.noreply.github.com>
2020-12-03 16:26:48 +0000

    quiz.py

--- OOP/quiz.py
+++ OOP/quiz.py
@@ -4,26 +4,35 @@ import random
 class Question:
 	def __init__(self, diff: int):
 		self.diff = diff
-		self.question, self.answers = None, None
+		self.question, self.answers, self.answer = None, None, None
 
-	def generateQuestion(self):
-		self.question = f"{random.randint(1, 10 ** self.diff)} {random.choice('+-*')} {random.randint(1, 10 ** self.diff)}"
-		tmpAnswer = self.generateAnswer()
-		self.answers = {round(tmpAnswer * random.random()) for _ in range(3)} | {tmpAnswer}
-		del tmpAnswer
+	def generate(self):
+		num1, num2, op = random.randint(1, 10 ** self.diff), random.randint(1, 10 ** self.diff), random.choice('+-*')
+		self.question = f"{num1} {op} {num2}"
+		self.answer = eval(self.question)
+		self.answers = [self.getRandom(num1, num2, op) for _ in range(3)]
+		self.answers.append(self.answer)
+		random.shuffle(self.answers)
 		return self.question, self.answers
 
-	def generateAnswer(self) -> int:
-		return eval(self.question)
+	@staticmethod
+	def getRandom(num1: int, num2: int, op: str):
+		low1, high1 = num1 - 2, num1 + 2
+		low2, high2 = num2 - 2, num2 + 2
+		while True:
+			ans = eval(f"{random.choice(range(low1, high1))} {op} {random.choice(range(low2, high2))}")
+			if ans != eval(f"{num1}{op}{num2}"):
+				return ans
 
 
 score = 1
 for _ in range(int(input("How many questions>\n> "))):
-	question= Question(score)
-	q, answers = question.generateQuestion()
-	answers = "\n".join(f"{i}: {answers[i]}" for i in range(4))
-	answer = int(input(f"Question: {q}\nAnswers: {answers}"))
-	if answer == question.generateAnswer():
+	questionObj = Question(score)
+	question, answers = questionObj.generate()
+	answer = int(input(f"Question: {question}\nAnswers:\n"
+														+ "\n".join(f"{num + 1}: {answers[num]}" for num in range(4))
+														+ "\n> "))
+	if answer == answers.index(questionObj.answer) + 1:
 		print("Correct")
 		score += 1
 	else:
