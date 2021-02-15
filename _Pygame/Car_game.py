fe6d1ca974561c2e842b444b0a76afe0f0a159ff
Reddersc022 <64492350+Reddersc022@users.noreply.github.com>
2021-02-15 14:56:35 +0000

    Added death timer Added more enemies

--- _Pygame/Car_game.py
+++ _Pygame/Car_game.py
@@ -1,5 +1,5 @@
 """
-Game involving driving a car, if it hits the wall it dies
+Game involving controlling a player and avoiding a number of enemies
 
 Controls:
 	- W = Up
@@ -12,28 +12,31 @@ Controls:
 
 # Imports
 import pygame
-import numpy
+import random
 
 from typing import Tuple, List
 
 
 # Constants / variables
+# Colours
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREY = (211, 211, 211)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 
+# Text
 pygame.font.init()
 FONT = pygame.sysfont.SysFont("Comfortaa", 20)
 BIG_FONT = pygame.sysfont.SysFont("Comfortaa", 50)
 START_TEXT = "Use WASD to move and space to stop\nHitting a wall causes death".split("\n")
 DEATH_TEXT = "You have died".split("\n")
 
+# Game controls
 FPS = 120
 SIZE = (1200, 700)
 INF = 10000
-KEY_VALUES = {"w": -.01, "s": .01, "a": -.01, "d": .01}
+KEY_VALUES = {pygame.K_w: (0, -.01), pygame.K_s: (0, .01), pygame.K_a: (-.01, 0), pygame.K_d: (.01, 0)}
 
 
 # Boundary class
@@ -57,14 +60,15 @@ class Boundary:
 
 # Player class
 class Player:
-	def __init__(self, max_speed: int, colour: Tuple[int, int, int] = BLACK):
+	def __init__(self, max_speed: float, colour: Tuple[int, int, int] = BLACK):
 		self.speeds = [0, 0]
 		self.coords = [20, 20]
 		self.maxSpeed = max_speed
 		self.colour = colour
+		self.hit = False
 
 	def update(self, change_x: int, change_y: int, *boundaries):
-		# Sort speeds to be under max
+		# Sort speeds to be under max, keep to 2 dp
 		self.speeds[0] = round(self.speeds[0] + change_x, 2)
 		if self.speeds[0] > self.maxSpeed:
 			self.speeds[0] = self.maxSpeed
@@ -89,24 +93,30 @@ class Player:
 
 
 class Enemy:
-	def __init__(self, max_speed: int, colour: Tuple[int, int, int] = RED):
+	enemyNum = 0
+
+	def __init__(self, max_speed: float, colour: Tuple[int, int, int] = RED):
 		self.maxSpeed = max_speed
 		self.colour = colour
 		self.coords = [1000, 600]
+		self.number = Enemy.enemyNum
+		Enemy.enemyNum += 1
 
 	def update(self, player_coords: List[int], *boundaries):
-		"""Will move enemy toward player, using a unit vector for the direction"""
+		"""Will move enemy toward player, using a unit vector for the direction. Adds in some random variation too"""
 		# TODO: Make avoid boundaries
-		# Calculate vector
+		# Calculate differences in coords
 		diffX, diffY = player_coords[0] - self.coords[0], player_coords[1] - self.coords[1]
+		# Calculate magnitude of vector
 		vectorMag = pow(pow(abs(diffX), 2) + pow(abs(diffY), 2), .5)
+		# Apply magnitude to create unit vector
 		try:
-			changeX = (diffX / vectorMag) * self.maxSpeed
+			changeX = (diffX / vectorMag) * self.maxSpeed * random.uniform(.5, 1.5)
 		except ZeroDivisionError:
 			changeX = 0
 
 		try:
-			changeY = (diffY / vectorMag) * self.maxSpeed
+			changeY = (diffY / vectorMag) * self.maxSpeed * random.uniform(.5, 1.5)
 		except ZeroDivisionError:
 			changeY = 0
 
@@ -124,12 +134,12 @@ class Enemy:
 			self.coords = [round(tmpX, 2), round(tmpY, 2)]
 
 
-# Pygame loop
+# Pygame setup
 pygame.init()
 screen = pygame.display.set_mode(SIZE)
 clock = pygame.time.Clock()
 
-# Game variables
+# Boundaries
 BOUNDARIES = [
 	Boundary((-10, -INF), (SIZE[0]+10, -INF), (SIZE[0]+10, 0), (-10, 0)),  # Top
 	Boundary((-10, SIZE[1]), (SIZE[0]+10, SIZE[1]), (SIZE[0]+10, SIZE[1]+INF), (-10, SIZE[1]+INF)),  # Bottom
@@ -140,57 +150,66 @@ BOUNDARIES = [
 	Boundary((100, 350), (700, 350), (700, 450), (100, 450))
 ]
 
+# Player and enemies
 player = Player(5)
-enemies = [Enemy(1, RED)]
+enemies = [Enemy(1, RED), Enemy(1.5, RED), Enemy(1.3, RED)]
+touches = [0 for _ in range(len(enemies))]
 
+# Game variables
 done, keypress, dead = False, False, False
-keysDown = {"w": False, "s": False, "a": False, "d": False}
+keysDown = {pygame.K_w: False, pygame.K_s: False, pygame.K_a: False, pygame.K_d: False}
+aliveTime = 0
 while not done:
-	enemyMove = False
-	changeX, changeY = 0, 0
+	directions = [0, 0]
 	for ev in pygame.event.get():
 		if ev.type == pygame.QUIT:  # Quit
 			done = True
 
 		if not dead:
-			if ev.type == pygame.KEYDOWN:  # Keypress
-				keypress, enemyMove = True, True
-				for key in "wasd":
-					if eval(f"ev.key == pygame.K_{key}"):
-						keysDown[key] = True
+			# If can move, sort key presses
+			if ev.type == pygame.KEYDOWN:
+				if ev.key in keysDown:
+					# Enemy moves when you do
+					keypress= True
+					# Set key to pressed
+					keysDown[ev.key] = True
+					# Sort space
 				if ev.key == pygame.K_SPACE:
 					player.speeds = [0, 0]
 
 			elif ev.type == pygame.KEYUP:
-				for key in "wasd":
-					if eval(f"ev.key == pygame.K_{key}"):
-						keysDown[key] = False
+				if ev.key in keysDown:
+					# Key no longer pressed
+					keysDown[ev.key] = False
 
 	if not dead:
-		# Sort key presses
-		for key in "ws":
-			if keysDown[key]:
-				changeY = KEY_VALUES[key]
+		# Add to alive time
+		aliveTime += 1
 
-		for key in "ad":
+		# Sort keys still pressed
+		for key in keysDown:
 			if keysDown[key]:
-				changeX = KEY_VALUES[key]
+				directions[0] += KEY_VALUES[key][0]
+				directions[1] += KEY_VALUES[key][1]
 
+		# Clean screen to draw
 		screen.fill(GREY)
 
-		# Update player and enemy
-		player.update(changeX, changeY, *BOUNDARIES)
+		# Update player and enemies
+		player.update(*directions, *BOUNDARIES)
 		pygame.draw.rect(screen, player.colour, (player.coords[0]-10, player.coords[1]-10, 20, 20))
 
 		for enemy in enemies:
-			enemy.update(player.coords, *BOUNDARIES)
+			if any(keysDown.values()):
+				enemy.update(player.coords, *BOUNDARIES)
 			pygame.draw.rect(screen, enemy.colour, (enemy.coords[0]-10, enemy.coords[1]-10, 20, 20))
 
 			# Check if player has been hit
 			if player.coords[0]-2 < enemy.coords[0] < player.coords[0]+22\
 					and player.coords[1]-2 < enemy.coords[1] < player.coords[1]+22:
-				dead = True
-				player.speeds = [0, 0]
+				touches[enemy.number] += 1
+			else:
+				touches[enemy.number] = 0
 
 	# Displaying text
 	if not keypress:
@@ -208,12 +227,21 @@ while not done:
 		screen.blit(speedYText, (10, 22))
 		screen.blit(coordsText, (10, 34))
 
+		# Death timer
+		deathText = FONT.render(f"Death timer: {round(max(touches)/FPS, 2)}s", 3, BLACK)
+		screen.blit(deathText, (10, 46))
+
+		# Alive timer
+		aliveText = FONT.render(f"Alive timer: {round(aliveTime/FPS, 2)}s", 4, BLACK)
+		screen.blit(aliveText, (10, 58))
+
 	# Draw boundaries
 	for bound in BOUNDARIES:
 		pygame.draw.polygon(screen, bound.colour, bound.pointList)
 
-	# Draw death text now (over boundaries)
-	if dead:
+	# Draw death text now (over boundaries), die after one second of touch
+	if max(touches) >= FPS:
+		dead = True
 		line = BIG_FONT.render("You have died", -10, BLACK)
 		screen.blit(line, (SIZE[0]/2 - 60, SIZE[1]/2 - 20))
 
