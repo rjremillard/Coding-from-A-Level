2ba34da76247b5aea82c4c0057364db0258d0ea6
Reddersc022 <64492350+Reddersc022@users.noreply.github.com>
2021-02-23 10:56:50 +0000

    Fixed age

--- Machine_learning/Titanic/main.py
+++ Machine_learning/Titanic/main.py
@@ -9,13 +9,16 @@ DATA = pandas.read_csv("train.csv")
 for i in ["Name", "Ticket", "Cabin"]:
 	del DATA[i]
 
+# Give default age
+DATA["Age"] = DATA["Age"].apply(lambda x: 30 if x == np.NAN else x)
+
+# Now get rid of NaNs
+DATA = DATA.dropna()
+
 # Map other qualitative data to quantitative
 maps = {"Sex": {"male": 0, "female": 1}, "Embarked": {"S": 0, "C": 1, "Q": 2}}
 for i in maps.keys():
 	DATA[i] = DATA[i].apply(lambda x: maps[i][x])
-
-# Give default age
-DATA["Age"] = DATA["Age"].apply(lambda x: 36 if x == np.NAN else x)
 
 # Get averages for each header
 AVERAGES = {}
