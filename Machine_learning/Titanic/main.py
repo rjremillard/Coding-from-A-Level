437047ca8c590885ce5e6a420f55efe565e5a1ef
Reddersc022 <64492350+Reddersc022@users.noreply.github.com>
2021-02-23 19:08:38 +0000

    Now using tree

--- Machine_learning/Titanic/main.py
+++ Machine_learning/Titanic/main.py
@@ -4,32 +4,29 @@ import pandas
 import numpy as np
 import re
 
-from collections import defaultdict
+from sklearn.tree import DecisionTreeRegressor
 
-# Between indexes and titles
-IND_TITLE = {0: "PassengerId", 1: "Survived", 2: "Pclass", 3: "Name", 4: "Sex", 5: "Age", 6: "SibSp", 7: "Parch",
-	8: "Fare", 9: "Embarked"}
 
-# Get data
+# --- Get and clean data ---
 data = pandas.read_csv("train.csv")
 # Removing unneeded columns and empty boxes
 for i in ["Ticket", "Cabin"]:
 	del data[i]
 
-
-# Set names to titles, use regex
-def getTitle(name: str) -> str:
-	search = re.search(r"(\w+(?=\.))", name)
-	return search.group()
-
-
-data["Name"] = data["Name"].apply(getTitle)
-
-# Give default age
-data["Age"] = data["Age"].apply(lambda x: 30 if x == np.NAN else x)
+# # Set names to titles, use regex
+# def getTitle(name: str) -> str:
+# 	# All titles in the file
+# 	search = re.search(r"(\w+(?=\.))", name)
+# 	return search.group()
+#
+#
+# data["Name"] = data["Name"].apply(getTitle)
+#
+# # Give default age
+# data["Age"] = data["Age"].apply(lambda x: 30 if x == np.NAN else x)
 
 # Now get rid of NaNs
-data = data.dropna()
+data = data.dropna(axis=0)
 
 # Map other qualitative data to quantitative
 maps = {"Sex": {"male": 0, "female": 1}, "Embarked": {"S": 0, "C": 1, "Q": 2}}
@@ -37,3 +34,17 @@ for i in maps.keys():
 	data[i] = data[i].apply(lambda x: maps[i][x])
 
 
+# --- Process Data ---
+# Get features and target
+y = data.Survived
+
+features = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
+X = data[features]
+X.head()
+
+model = DecisionTreeRegressor(random_state=1)
+model.fit(X, y)
+
+# Apply
+print(X.head())
+print(model.predict(X.head()))
