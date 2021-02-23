742cdd826a6bf8314fdb2154e6902c2d4a2c07cf
Reddersc022 <64492350+Reddersc022@users.noreply.github.com>
2021-02-23 16:16:37 +0000

    Restart

--- Machine_learning/Titanic/main.py
+++ Machine_learning/Titanic/main.py
@@ -6,57 +6,34 @@ import re
 
 from collections import defaultdict
 
+# Between indexes and titles
+IND_TITLE = {0: "PassengerId", 1: "Survived", 2: "Pclass", 3: "Name", 4: "Sex", 5: "Age", 6: "SibSp", 7: "Parch",
+	8: "Fare", 9: "Embarked"}
+
 # Get data
-DATA = pandas.read_csv("train.csv")
+data = pandas.read_csv("train.csv")
 # Removing unneeded columns and empty boxes
 for i in ["Ticket", "Cabin"]:
-	del DATA[i]
+	del data[i]
 
 
 # Set names to titles, use regex
 def getTitle(name: str) -> str:
-	# All titles in the file
 	search = re.search(r"(\w+(?=\.))", name)
 	return search.group()
 
 
-DATA["Name"] = DATA["Name"].apply(getTitle)
+data["Name"] = data["Name"].apply(getTitle)
 
 # Give default age
-DATA["Age"] = DATA["Age"].apply(lambda x: 30 if x == np.NAN else x)
+data["Age"] = data["Age"].apply(lambda x: 30 if x == np.NAN else x)
 
 # Now get rid of NaNs
-DATA = DATA.dropna()
+data = data.dropna()
 
 # Map other qualitative data to quantitative
 maps = {"Sex": {"male": 0, "female": 1}, "Embarked": {"S": 0, "C": 1, "Q": 2}}
 for i in maps.keys():
-	DATA[i] = DATA[i].apply(lambda x: maps[i][x])
+	data[i] = data[i].apply(lambda x: maps[i][x])
 
-# Get averages for each header
-AVERAGES = []
-for column in DATA.columns:
-	if column == "Name":
-		# Most common as is qualitative
-		AVERAGES.append(DATA[column].mode().values[0])
-	else:
-		AVERAGES.append(DATA[column].mean())
 
-print(AVERAGES)
-
-weights = defaultdict(lambda: 0)
-# Iterate through rows and update weights
-for passenger in DATA.iterrows():
-	passenger = passenger[1]
-	survived = passenger[1]
-	for i in range(2, len(DATA.columns)):
-		if i == 3:
-			pass
-		else:
-			if survived:
-				if passenger[i] > AVERAGES[i]:
-					weights[i] += .1
-				else:
-					weights[i] -= .1
-
-print(weights)
