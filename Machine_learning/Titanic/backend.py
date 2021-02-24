4e3314bf8d80debe2ed735120a8bb083274cf6fa
Reddersc022 <64492350+Reddersc022@users.noreply.github.com>
2021-02-24 12:53:37 +0000

    First part of GUI done

--- Machine_learning/Titanic/backend.py
+++ Machine_learning/Titanic/backend.py
@@ -1,8 +1,4 @@
-"""Creates tree for use in `frontend.py`
-
-- If imported, will create the tree to be used and delete other variables. Tree can be accessed in variable `model`
-- If run independently, will test itself - results in STDOUT
-"""
+"""Creates tree for use in `frontend.py`"""
 
 import pandas
 import re
@@ -10,52 +6,59 @@ import numpy as np
 
 from sklearn.tree import DecisionTreeRegressor
 
-# --- Constants ---
-TO_REMOVE = ["Ticket", "Cabin"]
-MAPS = {"Sex": {"male": 0, "female": 1}, "Embarked": {"S": 0, "C": 1, "Q": 2}}
-# TODO: Make names matter
-FEATURES = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
 
+def makeModel(used_internally=False):
+	""":param used_internally: whether it returns X and y"""
+	# --- Constants ---
+	TO_REMOVE = ["Ticket", "Cabin"]
+	MAPS = {"Sex": {"male": 0, "female": 1}, "Embarked": {"S": 0, "C": 1, "Q": 2}}
+	# TODO: Make names matter
+	FEATURES = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
 
-# --- Get and clean data ---
-data = pandas.read_csv("train.csv")
-# Removing unneeded columns and empty boxes
-for i in TO_REMOVE:
-	del data[i]
+	# --- Get and clean data ---
+	data = pandas.read_csv("train.csv")
+	# Removing unneeded columns and empty boxes
+	for i in TO_REMOVE:
+		del data[i]
 
+	# Set names to titles, use regex
+	def getTitleNum(name: str) -> str:
+		# All titles in the file
+		search = re.search(r"(\w+(?=\.))", name)
+		return search.group()
 
-# Set names to titles, use regex
-def getTitleNum(name: str) -> str:
-	# All titles in the file
-	search = re.search(r"(\w+(?=\.))", name)
-	return search.group()
+	data["Name"] = data["Name"].map(getTitleNum)
 
+	# Give default age
+	data["Age"] = data["Age"].map(lambda x: 30 if x == np.NAN else x)
 
-data["Name"] = data["Name"].map(getTitleNum)
+	# Now get rid of NaNs
+	data = data.dropna(axis=0)
 
-# Give default age
-data["Age"] = data["Age"].map(lambda x: 30 if x == np.NAN else x)
+	# Map other qualitative data to quantitative
+	for i in MAPS.keys():
+		data[i] = data[i].map(lambda x: MAPS[i][x])
 
-# Now get rid of NaNs
-data = data.dropna(axis=0)
+	# --- Process Data and Make Model ---
+	# Get features and target
+	y_ = data.Survived
+	X_ = data[FEATURES]
 
-# Map other qualitative data to quantitative
-for i in MAPS.keys():
-	data[i] = data[i].map(lambda x: MAPS[i][x])
+	# Make model
+	model_ = DecisionTreeRegressor(random_state=1)
+	model_.fit(X_, y_)
 
-
-# --- Process Data and Make Model ---
-# Get features and target
-y = data.Survived
-X = data[FEATURES]
-
-# Make model
-model = DecisionTreeRegressor(random_state=1)
-model.fit(X, y)
+	if used_internally:
+		return model_, X_, y_
+	else:
+		return model_
 
 
 # --- Sort Out the Rest ---
 if __name__ == "__main__":
+	# Get model and variables
+	model, X, y = makeModel(used_internally=True)
+
 	# Test the model
 	test_return = model.predict(X)
 	print(f"""
@@ -64,6 +67,3 @@ if __name__ == "__main__":
 		Num of actual survivors:    {sum(y)}
 		Num of predicted survivors: {round(sum(test_return))}
 	""")
-else:
-	# Clean up
-	del TO_REMOVE, MAPS, FEATURES, data, y, X
--- Machine_learning/Titanic/frontend.py
+++ Machine_learning/Titanic/frontend.py
@@ -2,6 +2,8 @@
 
 import tkinter
 import backend
+import pandas
+import re
 
 from tkinter import messagebox
 
@@ -10,6 +12,10 @@ from tkinter import messagebox
 FONT10 = ("comfortaa", 10)
 FONT11 = ("comfortaa", 11)
 PADS = {"padx": 10, "pady": 10}
+
+
+# --- Get model ---
+model = backend.makeModel()
 
 
 # --- Tkinter GUI ---
@@ -24,55 +30,85 @@ title.grid(column=2, columnspan=5, row=0, **PADS)
 titleL = tkinter.Label(master=window, text="User Input", font=FONT10)
 titleL.grid(column=0, columnspan=2, row=1, **PADS)
 
-labelL0 = tkinter.Label(master=window, text="Pclass: ", font=FONT10)
+labelL0 = tkinter.Label(master=window, text="pClass: ", font=FONT10)
 labelL0.grid(column=0, row=2, **PADS)
-entry0 = tkinter.Entry(master=window, font=FONT10)
-entry0.grid(column=1, row=2, **PADS)
-labelL1 = tkinter.Label(master=window, text="Name: ", font=FONT10)
+entryL0 = tkinter.Entry(master=window, font=FONT10)
+entryL0.grid(column=1, row=2, **PADS)
+labelL1 = tkinter.Label(master=window, text="name: ", font=FONT10)
 labelL1.grid(column=0, row=3, **PADS)
-entry1 = tkinter.Entry(master=window, font=FONT10)
-entry1.grid(column=1, row=3, **PADS)
-labelL2 = tkinter.Label(master=window, text="Sex: ", font=FONT10)
+entryL1 = tkinter.Entry(master=window, font=FONT10)
+entryL1.grid(column=1, row=3, **PADS)
+labelL2 = tkinter.Label(master=window, text="sex: ", font=FONT10)
 labelL2.grid(column=0, row=4, **PADS)
-entry2 = tkinter.Entry(master=window, font=FONT10)
-entry2.grid(column=1, row=4, **PADS)
-labelL3 = tkinter.Label(master=window, text="Age: ", font=FONT10)
+entryL2 = tkinter.Entry(master=window, font=FONT10)
+entryL2.grid(column=1, row=4, **PADS)
+labelL3 = tkinter.Label(master=window, text="age: ", font=FONT10)
 labelL3.grid(column=0, row=5, **PADS)
-entry3 = tkinter.Entry(master=window, font=FONT10)
-entry3.grid(column=1, row=5, **PADS)
-labelL4 = tkinter.Label(master=window, text="SibSp: ", font=FONT10)
+entryL3 = tkinter.Entry(master=window, font=FONT10)
+entryL3.grid(column=1, row=5, **PADS)
+labelL4 = tkinter.Label(master=window, text="sibSp: ", font=FONT10)
 labelL4.grid(column=0, row=6, **PADS)
-entry4 = tkinter.Entry(master=window, font=FONT10)
-entry4.grid(column=1, row=6, **PADS)
-labelL5 = tkinter.Label(master=window, text="Parch: ", font=FONT10)
+entryL4 = tkinter.Entry(master=window, font=FONT10)
+entryL4.grid(column=1, row=6, **PADS)
+labelL5 = tkinter.Label(master=window, text="parch: ", font=FONT10)
 labelL5.grid(column=0, row=7, **PADS)
-entry5 = tkinter.Entry(master=window, font=FONT10)
-entry5.grid(column=1, row=7, **PADS)
-labelL6 = tkinter.Label(master=window, text="Fare: ", font=FONT10)
+entryL5 = tkinter.Entry(master=window, font=FONT10)
+entryL5.grid(column=1, row=7, **PADS)
+labelL6 = tkinter.Label(master=window, text="fare: ", font=FONT10)
 labelL6.grid(column=0, row=8, **PADS)
-entry6 = tkinter.Entry(master=window, font=FONT10)
-entry6.grid(column=1, row=8, **PADS)
-labelL7 = tkinter.Label(master=window, text="Embarked: ", font=FONT10)
+entryL6 = tkinter.Entry(master=window, font=FONT10)
+entryL6.grid(column=1, row=8, **PADS)
+labelL7 = tkinter.Label(master=window, text="embarked: ", font=FONT10)
 labelL7.grid(column=0, row=9, **PADS)
-entry7 = tkinter.Entry(master=window, font=FONT10)
-entry7.grid(column=1, row=9, **PADS)
+entryL7 = tkinter.Entry(master=window, font=FONT10)
+entryL7.grid(column=1, row=9, **PADS)
+
+
+def inputHelp():
+	messagebox.showinfo(
+		"Input Help",
+		"""
+pClass   :: The passenger's class (1-3)
+name     :: The passenger's name
+sex      :: The passenger's sex (male / female) (apologies to other sexes)
+age      :: The passenger's age (0+)
+sibSp    :: The number of siblings of the passenger, onboard (0+)
+parch    :: The number of parents, grandparents, etc... of the passenger, onboard (0+)
+fare     :: How much the passenger payed for the trip (0+)
+embarked :: Where the passenger embarked (S/C/Q)
+""")
 
 
 def predict():
-	entries = entry0.get(), entry1.get(), entry2.get(), entry3.get(), entry4.get(), entry5.get(), entry6.get(), entry7.get()
+	entries = [entryL0.get(), entryL1.get(), entryL2.get(), entryL3.get(), entryL4.get(), entryL5.get(), entryL6.get(),
+		entryL7.get()]
 	if all(map(len, entries)):
 		pClass, name, sex, age, sibSp, parch, fare, embarked = entries
-		# We know they're all there, check they're all acceptable
+		# We know they're all there, now check they're all acceptable
 		if all(map(lambda x: x.isnumeric(), [pClass, age, sibSp, parch, fare]))\
-				and all(map(lambda x: x.isalpha(), [name, sex, embarked])):
-			pass
+				and all(map(lambda x: bool(re.match(r"[\w.]+", x)), [name, sex, embarked])):
+			# Clean data
+			try:
+				sex = {"male": 0, "female": 1}[sex]
+				embarked = {"S": 0, "C": 1, "Q": 2}[embarked]
+			except KeyError:
+				messagebox.showerror("Invalid Input", "Either `sex` or `embarked` aren't legal inputs\nPlease consult the input help")
+			else:
+				data = [[pClass, sex, age, sibSp, parch, fare, embarked]]
+				prediction = model.predict(data)
+				messagebox.showinfo("Prediction", f"Your passenger {'will' if prediction else 'wont'} survive ({prediction[0]}%)")
 		else:
 			messagebox.showerror("Invalid Input", "One, or more, inputs are of the wrong type\nPlease consult the input help")
 	else:
 		messagebox.showerror("Invalid Input", "One, or more, inputs are missing")
 
 
+inpHelp = tkinter.Button(master=window, text="Input Help", command=inputHelp, font=FONT10)
+inpHelp.grid(column=0, row=10, **PADS)
 submit = tkinter.Button(master=window, text="Submit", command=predict, font=FONT10)
-submit.grid(column=0, columnspan=2, row=10, **PADS)
+submit.grid(column=1, row=10, **PADS)
+
+# Right (from file)
+
 
 tkinter.mainloop()
