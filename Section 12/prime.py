94f6f77d7ecdd233bcd1b115f2d401af3a0689f0
Reddersc022 <64492350+Reddersc022@users.noreply.github.com>
2021-04-13 21:21:32 +0100

    prime numbers, again...

--- Section 12/prime.py
+++ Section 12/prime.py
@@ -1,3 +1,6 @@
+import math
+
+
 def bruteForce(num: int) -> bool:
 	for i in range(2, num):
 		if not num % i:
@@ -7,16 +10,18 @@ def bruteForce(num: int) -> bool:
 
 
 def sieveOfE(num: int) -> list:
-	numbers = list(range(2, num+1))
-	for i in range(2, int(num ** .5) + 1):
-		if i in numbers:
-			for j in range(i**2, num + 1, i):
-				if j in numbers:
-					numbers.remove(j)
-	return numbers
+	numbers = [True for _ in range(num+1)]
+	for i in range(2, math.floor(math.sqrt(num)) + 1):
+		if numbers[i]:
+			j = i ** 2
+			while j < num:
+				numbers[j] = False
+				j += i
+	return [index+2 for index, prime in enumerate(numbers[2:-1]) if prime]
 
 
 if __name__ == "__main__":
 	x = int(input("Number: "))
 	print(f"{x} is {'prime' if bruteForce(x) else 'not prime'}")
-	print(f"Primes below: {sieveOfE(x)}")
+
+	print(f"Primes below {x}: {', '.join(map(str, sieveOfE(x)))}")
