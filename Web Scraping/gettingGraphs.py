ae35cfa213fb78fb4d423df2e1ba10e012b620b1
Reddersc022 <64492350+Reddersc022@users.noreply.github.com>
2021-05-25 16:06:41 +0100

    added percent

--- Web Scraping/gettingGraphs.py
+++ Web Scraping/gettingGraphs.py
@@ -1,9 +1,10 @@
 from requests import get
 from collections import defaultdict
 
-def getData(url: str) -> dict:
+def getData(url: str, percent=100) -> dict:
     """Gets data at url
     :param url: url to get, hopefully `https://www.cs.utah.edu/~lifeifei/research/tpq/cal.cedge`, or similar
+    :param percent: Optional - how much of the data should be considered
     :return adjacency: in form {node#: {neighbour#: weight, ...}, ...}, note that the graph is weighted
     """
     resp = get(url)
@@ -11,10 +12,10 @@ def getData(url: str) -> dict:
     if resp.status_code == 200:
         data = [
             line.split() for line in resp.text.split("\n")
-        ]
+        ][:-1]
 
         adjacency = defaultdict(lambda: defaultdict(int))
-        for vertex in data[:-1]:  # Avoid last (empty)
+        for vertex in data[:int(len(data) * (percent/100))]:  # Avoid last (empty)
             # Vertex: [vertex#, start#, end#, weight]
             adjacency[vertex[1]][vertex[2]] = vertex[3]
 
